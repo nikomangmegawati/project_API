2a2cba1de32ba8f79ac052d47ee2c7ccd79dac4a
nikomangmegawati <nikomangmegawati1299@gmail.com>
2020-05-10 16:18:19 +0800

    API project

---
+++ lib/src/app.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud_api_sample_app/src/ui/formadd/form_add_screen.dart';
+import 'package:flutter_crud_api_sample_app/src/ui/home/home_screen.dart';
+
+GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
+
+class App extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.greenAccent,
+        accentColor: Colors.greenAccent,
+
+      ),
+      home: Scaffold(
+        key: _scaffoldState,
+        appBar: AppBar(
+          title: Text(
+            "Daftar Nama",
+            style: TextStyle(
+
+              fontSize: 30.0,
+              color: Colors.white,
+            ),
+          ),
+          flexibleSpace: Container (
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.red,
+                  Colors.yellowAccent,
+                ]
+              ),
+            ),
+          ),
+          actions: <Widget>[
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  _scaffoldState.currentContext,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                    return FormAddScreen();
+                  }),
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: Icon(
+                  Icons.add_box,
+                  color: Colors.white,
+
+
+                ),
+              ),
+            ),
+          ],
+        ),
+        body:
+        HomeScreen(),
+      ),
+    );
+  }
+}
